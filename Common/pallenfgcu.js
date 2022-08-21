0b0d8d284f4b9477c9ff2a01ccc8ea3b7c80dd99
Paul Allen <pallen@fgcu.edu>
2022-08-21 09:15:17 -0400

    Move main sidbar nav to Common at root site level and import to course through jquery (testing with CDA 3104).

--- Common/pallenfgcu.js
+++ Common/pallenfgcu.js
@@ -1,6 +1,6 @@
 // Side navigation
 function w3_open() {
-    var x = document.getElementById("mySidebar");
+    var x = document.getElementById("navSidebar");
     x.style.width = "100%";
     x.style.fontSize = "40px";
     x.style.paddingTop = "10%";
@@ -8,5 +8,5 @@ function w3_open() {
 }
 
 function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("navSidebar").style.display = "none";
 }
